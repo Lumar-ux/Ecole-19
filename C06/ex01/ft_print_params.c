0ac5dc85d75c7a127689323966adafa7cb43383d
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-18 23:09:15 +0200

    Add files via upload

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -2,51 +2,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void    ft_wr(char c)
-{
-    write (1, &c, 1);
-}
-
 int main(int argc, char *argv[])
 {
     int i;
+    int n;
 
+    n = 1;
+
+    while(n < argc)
+    {
     i = 0;
-    while (argv[i] != '\0' && 3 > argc - 1)
-    {
-		if(i < 3)
-		{
-        ft_wr(*argv[i]);
-		i++;
-		}
-   	}
-    ft_wr ('\n');
-    return (0);
-}
-
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-void    ft_wr(char c)
-{
-    write (1, &c, 1);
-}
-
-int main(int argc, char *argv[])
-{
-    int i;
-
-    i = 1;
-if(argc <= 3)
-{
-    while (argv[1][i] != '\0')
-    {
-        write (1, &argv[1][i], 1);
+    while (argv[n][i] != '\0')
+        {
+        write (1, &argv[n][i], 1);
 		i++;
 		}
 
     write (1, "\n", 1);
-}
+    n++;
+    }
     return (0);
 }
