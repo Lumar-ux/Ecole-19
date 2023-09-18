e95656a3b715018998cef3d2be9135b574e48695
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-18 19:24:45 +0200

    Add files via upload

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmaroy <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 18:33:50 by lmaroy            #+#    #+#             */
+/*   Updated: 2023/09/18 19:22:45 by lmaroy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*
+void	ft_wr(char c)
+{
+	write (1, &c, 1);
+}*/
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int ag;
+
+	i = 0;
+	*ag = argv[i];
+
+	/* while (ag != '\0' && argc)
+	{*/
+	write (1, &ag\n, e);
+
+//	ft_wr ('\n');
+	return (0);
+}
