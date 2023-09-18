8ed25852d7b311102c97c00f89a2c168275186dc
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-18 20:41:21 +0200

    Update ft_print_params.c

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -23,3 +23,30 @@ int main(int argc, char *argv[])
     ft_wr ('\n');
     return (0);
 }
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void    ft_wr(char c)
+{
+    write (1, &c, 1);
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    i = 1;
+if(argc <= 3)
+{
+    while (argv[1][i] != '\0')
+    {
+        write (1, &argv[1][i], 1);
+		i++;
+		}
+
+    write (1, "\n", 1);
+}
+    return (0);
+}
