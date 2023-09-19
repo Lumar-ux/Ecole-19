fd847c16a420ebe46d2a8cf3b7c2d3f0274aabfa
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-19 15:31:11 +0200

    Update ft_sort_params_copy.c

--- C06/ex03/ft_sort_params_copy.c
+++ C06/ex03/ft_sort_params_copy.c
@@ -5,6 +5,7 @@ void ft_wr(int argc, char *argv[])
 {
     int i;
     int n;
+
     n = 1;
 while(n < argc )
     {
@@ -32,13 +33,11 @@ void ft_sort_params(int argc, char *argv[])
 {
    int i;
     int n;
-    //int j;
     int temp;
 
     n = 1;
     while(n < argc - 1)
     {
-    //n = j + 1;
     i = 0;
     while (argv[n][i] != '\0')
         {
@@ -49,36 +48,24 @@ void ft_sort_params(int argc, char *argv[])
                     argv[n][i] = argv[n + 1][i];
                     argv[n + 1][i] = temp;
                     n = 0;
-                    //n++;
-                    //j++;
                     } 
                     i++;   
                     n++;
             }
             i = 0;
 		}
-        //ft_wr(argc, argv);
-    if(argv[n][i] > 9)
+    if(argv[n][i] > '9')
 	{
-		ft_sort_params(argc / 10 , argv[n][i] / 10);
-		ft_sort_params(argc / 10, argv[n][i] % 10);
+		ft_sort_params(argc / 10, argv);
+		ft_sort_params(argc % 10, argv);
 	}
 	else
 	{
-		ft_wr(argc, argv[n][i] + 48);
+		ft_wr(argc, argv);
 	}
-    //return (0);
 }
 int main(int argc, char *argv[])
 {
-    int n;
-    int i;
-
-ft_sort_params(argc, argv[n][i]);
+ft_sort_params(argc, argv);
 return (0);
 }
-
-/*if((argv[n][i] >= 33) && (argv[n][i] <= 126))
-            {*/
-               /* if(argv[n + 1][i] > argv[n][i])
-                 n++;*/
