121b692cc5dcda610a433f841036ea2dd2520749
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-19 14:57:25 +0200

    Update ft_strlcpy.c

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -1,38 +1,77 @@
-#include <unistd.h>
 #include <stdio.h>
-
-unsigned *ft_strlcpy(char *dest, char *src, unsigned int size)
-     {
-    unsigned int i;
-	int src_len;	
-	
+#include <unistd.h>
+#include <stdlib.h>
+void ft_wr(int argc, char *argv[])
+{
+    int i;
+    int n;
+    n = 1;
+while(n < argc )
+    {
     i = 0;
-	src_len = 0;
-     while (i <  size - 1 && src[i] != '\0')
-     {
-	dest[i] = src[i];
-     i++;
-     }
-    
-        dest[i] = '\0';       
-	return(i);
+    while (argv[n][i] != '\0')
+        {
+        write (1, &argv[n][i], 1);
+		i++;
+		}
 
-	while (src[src_len] != '\0') 
-	{
-        src_len++;
+    write (1, "\n", 1);
+    n++;
     }
-  }
+}
+int diff(char *n1, char *n2)
+{
+    while(*n1 &&(*n1 == *n2))
+    {
+        n1++;
+        n2++;
+    }
+    return (*n1 - *n2);
+}
+int main(int argc, char *argv[])
+{
+    int i;
+    int n;
+    //int j;
+    int temp;
 
+    n = 1;
+    while(n < argc - 1)
+    {
+    //n = j + 1;
+    i = 0;
+    while (argv[n][i] != '\0')
+        {
+                 if ((argv[n][i] >= 32) && (argv[n][i] <= 127) 
+                    && diff(&argv[n][i], &argv[n + 1][i]) > 0)
+                    {
+                    temp = argv[n][i];
+                    argv[n][i] = argv[n + 1][i];
+                    argv[n + 1][i] = temp;
+                    n = 0;
+                    //n++;
+                    //j++;
+                    } 
+                    i++;   
+                    n++;
+            }
+            i = 0;
+		}
+        //ft_wr(argc, argv);
+    
+    if(argv[n][i] > 9)
+	{
+		main(argc, argv[n][i] / 10);
+		main(argc, argv[n][i] % 10);
+	}
+	else
+	{
+		ft_wr(argc, argv[n][i] + 48);
+	}
+    return (0);
+}
 
-/*int main(void)
-  {
-     char    y[] = "bonnjour";
-	 char    n[] = "boddnnjour";
-     char size = 12;
-     char dest[50];
-	 ft_strlcpy(dest, y, size);
-     printf("%zu",dest);
-	 printf("%zu", strlcpy(dest, y, size)); 
-	 printf("%zu", ft_strlcpy);
-     return (0);
- }*/
+/*if((argv[n][i] >= 33) && (argv[n][i] <= 126))
+            {*/
+               /* if(argv[n + 1][i] > argv[n][i])
+                 n++;*/
