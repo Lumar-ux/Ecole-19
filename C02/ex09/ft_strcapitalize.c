61156409e6feb1d78dc84f9afa9e950b25360e46
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-20 11:10:31 +0200

    Update ft_strcapitalize.c

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -21,7 +21,15 @@ return(str);
 
 int main(void)
 {
-char str1[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-printf("%s", ft_strcapitalize(str1));
+char str0[] = "Salut, Comment Tu Vas ? 42mots Quarante-Deux; Cinquante+Et+UN";
+char str1[] = "9,S46im2k9m-B,G#X";
+char str2[] = "NdHy.@$C@=;>$`Na+Ardz)@:E,=J<D*#";
+char str3[] = "OhO[Nf~";
+char str4[] = "Mp^Rf}@&N?Xb@X.]^{Kh'Tpf/Ya5ht;{Ply_Xr!-2";
+printf("%s", ft_strcapitalize(str0));
+printf("\n\n%s", ft_strcapitalize(str1));
+printf("\n\n%s", ft_strcapitalize(str2));
+printf("\n\n%s", ft_strcapitalize(str3));
+printf("\n\n%s", ft_strcapitalize(str4));
 return(0);
 }
