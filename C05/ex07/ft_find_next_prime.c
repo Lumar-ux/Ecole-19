d9302d7f8643a6ea1b82215c0c1b00383de85ae5
lmaroy <lucasmaroy42@gmail.com>
2023-09-23 23:29:23 +0200

    02

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
-void ft_check(int nb)
+/*void ft_check(int nb)
 {
-
+while ((((nb % 1) == 0) && ((nb % nb) == 0) && (nb % (2) != 0) && ((nb % (3) != 0))))
+{
+    nb++;
 }
+}*/
 int ft_find_next_prime(int nb)
 {
-    int check;
-    if (nb != 2 && nb != 1)
+    /*if (nb != 2 && nb != 1)
+    {*/
+    //ft_check(nb);
+    int div;
+
+    div = 2;
+    if(nb != 1 && nb != 0)
     {
-    if ((((nb % 1) == 0) && ((nb % nb) == 0) && (nb % (2) != 0) && ((nb % (3) != 0))))
-    { 
-    ft_check(nb);
+        while(nb > 0)
+        {
+            if ((((nb % div) == 0) && ((nb % nb) == 0) && (nb % (div) != 0)/* && ((nb % (div) != 0))*/))
+            {
+            div++;
+            /*if ((nb % (div) != 0) && (nb % (div) != 0))*/
+            {
+            return (nb);
+            }
+            }
+            if ((nb != 3) && (nb != 2))
+            {
+                nb++;
+            }
+            else
+            {    
+                return (nb);
+            }
+        }
     }
-    else
-    {    
-    return (0);
-    }
-    }
-return (nb);
+            return (nb);
 }
 int main()
 {
     int nb;
     int res1;
 
-    nb = 2;
-res1 = ft_is_prime(nb);
+    nb = 710;
+res1 = ft_find_next_prime(nb);
 printf("%d", res1);
 return (0);
 }
