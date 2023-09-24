989bc5d217fad765c37c667e1c7f7bb91c69646e
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-24 12:40:29 +0200

    Update Test01.c

--- C05/ex07/Test01.c
+++ C05/ex07/Test01.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
-int est_premier(int n) {
-    if (n <= 1) {
-        return 0;
+int ft_is_prime(int nb) {
+    if (nb < 2) {
+        return 0; // Pas premier
     }
-
-    int i = 2;
-    int estPremier = 1;
-    while (i * i <= n && estPremier) {
-        if (n % i == 0) {
-            estPremier = 0; 
+    for (int i = 2; i * i <= nb; i++) {
+        if (nb % i == 0) {
+            return 0; // Pas premier
         }
-        i++;
     }
+    return 1; // Premier
+}
 
-    return estPremier;
-
-int main(void) {
-    int num = 17;
-    if (est_premier(num)) {
-        printf("%d est premier\n", num);
-    } else {
-        printf("%d n'est pas premier\n", num);
+int ft_find_next_prime(int nb) {
+    if (nb < 2) {
+        return 2; // Le premier nombre premier est 2
     }
+    while (1) {
+        nb++;
+        if (ft_is_prime(nb)) {
+            return nb;
+        }
+    }
+}
+
+int main() {
+    int nb = 710;
+    int res1 = ft_find_next_prime(nb);
+    printf("Le prochain nombre premier après %d est : %d", nb, res1);
     return 0;
 }
