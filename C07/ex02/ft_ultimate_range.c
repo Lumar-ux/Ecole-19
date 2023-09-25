ff0bca18d499c03a47d3a3361389671c4959299e
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-25 21:44:39 +0200

    Add files via upload

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int ft_ultimate_range(int **range, int min, int max)
+{
+    int *tab;
+    int size;
+    int i;
+
+    i = 0;
+    
+if (min >= max)
+{
+    return (NULL);
+}
+size = max - min;
+range = malloc((size) * sizeof(int));
+if (range == NULL)
+{
+    return (0);
+}
+while (min < max)
+{
+    range[i] = min;
+    i++;
+    min++;
+}
+return (size);
+}
+int main()
+{
+    int i;
+    int min;
+    int max;
+    int nb;
+    //int size;
+    
+    i = 0;
+    min = 1;
+    max = 10;
+    //size = max - min;
+    int *res1 = ft_ultimate_range(nb, min, max);
+    while (i < nb)
+    {
+    printf("%d\n", res1[i]);
+    i++;
+    }
+    free(res1);
+    return (0);
+}
