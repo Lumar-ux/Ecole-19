1765ccbf84f2b9a31a456d8db71294c06fc5b34a
Lumar-ux <82873564+Lumar-ux@users.noreply.github.com>
2023-09-27 16:21:36 +0200

    Update ft_strjoin_2.c

--- C07/ex03/ft_strjoin_2.c
+++ C07/ex03/ft_strjoin_2.c
@@ -7,16 +7,16 @@ int ft_strlen(char **strs)
     int i = 0;
     int j = 0;
 
-    while (i < 3)
+    while (i <= 2)
     {
         while (strs[i][j] != '\0')
         {
-            j++;
+            j++;   
         }
         i++;
         j = 0;
     }
-    return i;
+    return (i);
 }
 
 char *ft_strjoin(int size, char **strs)
@@ -24,42 +24,45 @@ char *ft_strjoin(int size, char **strs)
     int total_len;
     int i;
     int j;
-    int index;
+    int in;
 
     i = 0;
     j = 0; 
-    index = 0;
+    in = 0;
     total_len = 0;
-    
-    char *result = (char *)malloc(total_len + 1);
-    if (result == NULL)
+    char *res = (char *)malloc((total_len) + 1 * sizeof(char));
+    if (res == NULL)
     {
         return NULL;
     }
     while (i < size)
     {
-        total_len = total_len + ft_strlen(&strs[i]);
-        i++;
+      while (strs[i][j] != '\0')
+      {
+        total_len = total_len + ft_strlen(&strs[j]);
+        j++;
+      }
+      i++;
     }
     while (i < size)
     {
-    i++;
         while (strs[i][j] != '\0')
         {
+        res[in++] = strs[i][j];
         j++;
-        result[index++] = strs[i][j];
         }
+        i++;
     }
-
-    result[index] = '\0';
-
-    return result;
+    res[in] = '\0';
+    return res;
 }
 
 int main()
 {
     int i = 0;
-    char **str1;
+    int j = 0;
+    char *str1[] = {"hello", "world" , "!"};
+    /*
     str1 = malloc((3) * sizeof(char));
     str1[0] = (char *)malloc((6) * sizeof(char));
     str1[1] = (char *)malloc((6) * sizeof(char));
@@ -67,18 +70,18 @@ int main()
     str1[0] = "hello";
     str1[1] = "world";
     str1[2] = "!";
-
-    int size = ft_strlen(&str1[0]) + ft_strlen(&str1[1]) + ft_strlen(&str1[3]);
-
-    char *res = ft_strjoin(size, str1);
+    */
+    char *res = ft_strjoin(ft_strlen(str1), str1);
 
     if (res != NULL)
     {
+    while (i < ft_strlen(&str1[i]))
+    {
         printf("%s\n", res);
-        free(str1);
-        free(str1[0]);
-        free(str1[1]);
-        free(str1[2]);
+        i++;
+        j++;
     }
+    }
+    free(res);
     return 0;
 }
